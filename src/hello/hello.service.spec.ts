ec9be6471a9b72c3641f22b501c5b96fa32a52e3
Vyacheslav Kopeyka <ks031088@gmail.com>
2026-05-02 16:10:00 +0500

    feat: add unit test for getHello function

--- src/hello/hello.service.spec.ts
+++ src/hello/hello.service.spec.ts
@@ -15,4 +15,8 @@ describe('HelloService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return \"Hello Wold!\"', () => {
+    expect(service.getHello()).toBe('Hello World!')
+  });
 });
