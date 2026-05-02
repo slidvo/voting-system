0adbbb0132597c819683af733dc56bb140f249c0
Vyacheslav Kopeyka <ks031088@gmail.com>
2026-05-02 15:57:15 +0500

    refactor: rename app.e2e to hello.e2e

--- test/hello.e2e-spec.ts
+++ test/hello.e2e-spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { HelloModule } from '../src/hello/hello.module';
 
-describe('AppController (e2e)', () => {
+describe('HelloController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [HelloModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
